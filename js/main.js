9f9a674ce91e67a98ff797b579f94342422e10fe
syed sadiq ali <syed sadiq ali>
2017-02-19 17:01:12 +0530

    final commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,49 @@
-$(document).ready(function(){
-	
+var getAQuote = function() {
+    $('h3').empty();
+    $('h4').empty();
+    $('h3').append('<i class="fa fa-spinner fa-spin fa-3x">');
+    $.ajax({
+        url: 'https://andruxnet-random-famous-quotes.p.mashape.com/cat=movies',
+        type: 'POST', // The HTTP Method
+        data: {}, // Additional parameters here
+        datatype: 'json',
+        success: function(data) {
+
+            obj = jQuery.parseJSON(data);
+
+            currentQuote = obj.quote;
+            currentAuthor = obj.author;
+            $('h3').empty();
+            $('h4').empty();
+            $('h3').append("\"" + obj.quote + "\"");
+            $('h4').append("-" + obj.author);
+        },
+        error: function(err) { alert(err); },
+        beforeSend: function(xhr) {
+            xhr.setRequestHeader("X-Mashape-Authorization", "2W1js31pYJmshjhSeK7bv8Sb4SIgp1HPxkDjsnxBZqr7ZhGKZ2"); // Enter here your Mashape key
+        }
+
+    });
+
+}
+
+
+
+var openthis = function() {
+
+    window.open('https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + encodeURIComponent('"' + currentQuote + '" ' + currentAuthor));
+}
+
+$(document).ready(function() {
+
+
+
+    $('#newquote').click(function() {
+
+        getAQuote();
+
+    });
+    $('#tweet').click(function() {
+        openthis();
+    });
 });
