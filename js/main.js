7c10ac52d423909f701742b28474edf76e8866b5
Syed Sadiq <youremail@example.com>
2017-02-25 20:31:01 +0530

    Some Changes

--- js/main.js
+++ js/main.js
@@ -3,20 +3,22 @@ var getAQuote = function() {
     $('h4').empty();
     $('h3').append('<i class="fa fa-spinner fa-spin fa-3x">');
     $.ajax({
-        url: 'https://andruxnet-random-famous-quotes.p.mashape.com/cat=movies',
-        type: 'POST', // The HTTP Method
+        url: 'https://andruxnet-random-famous-quotes.p.mashape.com/cat=famous',
+        type: 'GET', // The HTTP Method
         data: {}, // Additional parameters here
         datatype: 'json',
         success: function(data) {
-
             obj = jQuery.parseJSON(data);
-
             currentQuote = obj.quote;
             currentAuthor = obj.author;
-            $('h3').empty();
-            $('h4').empty();
-            $('h3').append("\"" + obj.quote + "\"");
-            $('h4').append("-" + obj.author);
+            var length = currentQuote.length + currentAuthor.length + 11;
+            if (length < 140 || length == 140) {
+                $('h3').empty();
+                $('h4').empty();
+                $('h3').append("\"" + obj.quote + "\"");
+                $('h4').append("-" + obj.author);
+            } else
+                getAQuote();
         },
         error: function(err) { alert(err); },
         beforeSend: function(xhr) {
@@ -26,24 +28,22 @@ var getAQuote = function() {
     });
 
 }
-
-
-
 var openthis = function() {
-
     window.open('https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + encodeURIComponent('"' + currentQuote + '" ' + currentAuthor));
 }
 
+
 $(document).ready(function() {
-
-
-
+    getAQuote();
     $('#newquote').click(function() {
 
         getAQuote();
-
     });
     $('#tweet').click(function() {
         openthis();
+
     });
+
+    
+
 });
